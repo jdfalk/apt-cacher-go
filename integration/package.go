9e63bd26174254b1d4d202a113a4697ca82189a6
Johnathan Falk <johnathan.falk@gmail.com>
2025-03-23 13:32:00 -0400

    feat: add integration package for testing functionality in apt-cacher-go
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

---
+++ integration/package.go
@@ -0,0 +1,5 @@
+// Package integration provides integration testing functionality for apt-cacher-go
+package integration
+
+// This file exists to make the package valid for Go's package system,
+// which requires at least one non-test Go file in a package directory.
