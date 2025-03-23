c8c1eab120e3cf1e28c2416547edfe7f27300192
Johnathan Falk <johnathan.falk@gmail.com>
2025-03-23 19:25:35 -0400

    feat: implement main command structure for apt-cacher-go
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jdfalk/apt-cacher-go/cmd/benchmark"
+	"github.com/jdfalk/apt-cacher-go/cmd/serve"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// Version information - to be set during build
+	Version   = "dev"
+	BuildTime = "unknown"
+	GitCommit = "unknown"
+
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "apt-cacher-go",
+		Short: "A caching proxy server for Debian/Ubuntu package repositories",
+		Long: `apt-cacher-go is a caching proxy server for Debian/Ubuntu package repositories.
+It helps reduce bandwidth usage by caching downloaded packages and serving them to multiple clients.`,
+		Version: Version,
+	}
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is /etc/apt-cacher-go/config.yaml)")
+
+	// Add subcommands
+	rootCmd.AddCommand(serve.NewCommand())
+	rootCmd.AddCommand(benchmark.NewCommand())
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Search config in standard locations
+		viper.AddConfigPath("/etc/apt-cacher-go/")
+		viper.AddConfigPath("$HOME/.apt-cacher-go")
+		viper.AddConfigPath(".")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+	}
+
+	// Read in environment variables that match
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("APT_CACHER")
+
+	// If a config file is found, read it in
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
